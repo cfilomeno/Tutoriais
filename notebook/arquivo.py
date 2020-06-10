a5a3f494634e2fbf85ab9f6b3e83caeed6603a9d
Cleber Filomeno <cleber.filomeno@gmail.com>
2020-06-10 13:51:53 -0300

    pycharm

--- notebook/arquivo.py
+++ notebook/arquivo.py
@@ -2,15 +2,27 @@ import os
 import math
 import random
 
+import inline as inline
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import optimize, signal
 from lmfit import models
+from werkzeug.utils import secure_filename
 
-xrd = pd.read_csv('LCCu1.txt', skipinitialspace=True, skiprows= 134)
-xrd.dropna(axis=1, inplace = True)
-xData = xrd['Angle']
-yData = xrd['Det1Disc1']
-plt.plot(xrd['Angle'], xrd['Det1Disc1'])
-plt.show()
+image = 'fig'
+
+
+def save_image_plot(fig1, figure_name):
+    filename = os.path.expanduser(f'{image}')
+    fig1.savefig(filename)
+    return filename
+
+
+xrd = pd.read_csv('LCCu1.txt', skipinitialspace=True, skiprows=134)
+xrd.dropna(axis=1, inplace=True)
+
+fig, ax = plt.subplots()
+ax.plot(xrd['Angle'], xrd['Det1Disc1'])
+save_image_plot(fig, 'xrd-LCCu1.png')
+
