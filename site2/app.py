ef7cf6a561eca8a2655c742d7e167a44638fc958
Cleber Filomeno <cleber.filomeno@gmail.com>
2020-06-16 14:18:59 -0300

    embending

--- site2/app.py
+++ site2/app.py
@@ -1,6 +1,9 @@
 import os
+
 import pandas as pd
 import json
+
+from bokeh.command.subcommands.json import JSON
 from flask import Flask, request, redirect, url_for, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 
@@ -8,7 +11,7 @@ from bokeh.plotting import figure, output_file
 from bokeh.io import show, save
 from bokeh.layouts import column
 from bokeh.models import ColumnDataSource, RangeTool
-from bokeh.embed import components, file_html, server_document
+from bokeh.embed import components, file_html, server_document, json_item
 from bokeh.resources import CDN
 
 
@@ -36,7 +39,7 @@ def uploaded_file(filename):
     df.dropna(axis=1, inplace=True)
     ColumnDataSource(df)
 
-    # output_file('/tmp/dif.html', title='Difratograma')
+    output_file('/tmp/dif.html', title='Difratograma')
     p = figure(plot_height=500, plot_width=1200, toolbar_location="right",
                x_axis_type="linear", x_axis_location="below",
                background_fill_color="#efefef", x_range=(df['Angle'].iloc[0], df['Angle'].iloc[-1]))
@@ -49,22 +52,17 @@ def uploaded_file(filename):
                     plot_height=130, plot_width=1200, y_range=p.y_range,
                     x_axis_type="linear", y_axis_type=None,
                     tools="", toolbar_location=None, background_fill_color="#efefef")
-
     range_tool = RangeTool(x_range=p.x_range)
     range_tool.overlay.fill_color = "navy"
     range_tool.overlay.fill_alpha = 0.2
-
     select.line('Angle', 'Det1Disc1', source=df)
     select.ygrid.grid_line_color = None
     select.xgrid.grid_line_color = None
     select.add_tools(range_tool)
     select.toolbar.active_multi = range_tool
 
-    plot = column(p, select)
-    file_html(plot, CDN, "/tmp/dif.html")
-    item_text = json.dumps(json_item(plot, "/tmp/dif"))
-    item = JSON.parse(item_text);
-    Bokeh.embed.embed_item(item)
+    save(column(p, select))
+    return render_template('page2.html')
 
 
 if __name__ == "__main__":
