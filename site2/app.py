e07d4353fefc52ab2d31b9f255a030994a0ca5e3
Cleber Filomeno <cleber.filomeno@gmail.com>
2020-06-22 12:32:28 -0300

    CDN
    https://youtu.be/mSI8UsOsORg

--- site2/app.py
+++ site2/app.py
@@ -2,13 +2,13 @@ import os
 import pandas as pd
 from flask import Flask, request, redirect, url_for, render_template, send_from_directory
 from werkzeug.utils import secure_filename
-
+import json
 import jinja2
 from bokeh.plotting import figure, output_file
 from bokeh.io import show, save
 from bokeh.layouts import column
 from bokeh.models import ColumnDataSource, RangeTool
-from bokeh.embed import components, file_html, server_document, json_item
+from bokeh.embed import components, file_html, server_document
 from bokeh.resources import CDN
 
 
@@ -35,7 +35,7 @@ def uploaded_file(filename):
     df = pd.read_csv(filepath, skipinitialspace=True, skiprows=134)
     df.dropna(axis=1, inplace=True)
     ColumnDataSource(df)
-    # output_file('/home/cleber/Documentos/GitHub/fitdrx/site2/templates/dif.html', mode='cdn')
+    output_file('/home/cleber/Documentos/GitHub/fitdrx/site2/templates/dif.html')
     p = figure(plot_height=500, plot_width=1200, toolbar_location="right",
                x_axis_type="linear", x_axis_location="below",
                background_fill_color="#efefef", x_range=(df['Angle'].iloc[0], df['Angle'].iloc[-1]))
@@ -43,40 +43,28 @@ def uploaded_file(filename):
     p.yaxis.axis_label = 'I (U.A.)'
     p.xaxis.axis_label = '2theta (º)'
 
-    # save(p)
-    script, div = components(p)
-    plot_script = script
-    plot_div = div
+    select = figure(title='Selecionar pico',
+                    plot_height=130, plot_width=1200, y_range=p.y_range,
+                    x_axis_type="linear", y_axis_type=None,
+                    tools="", toolbar_location=None, background_fill_color="#efefef")
+    range_tool = RangeTool(x_range=p.x_range)
+    range_tool.overlay.fill_color = "navy"
+    range_tool.overlay.fill_alpha = 0.2
+    select.line('Angle', 'Det1Disc1', source=df)
+    select.ygrid.grid_line_color = None
+    select.xgrid.grid_line_color = None
+    select.add_tools(range_tool)
+    select.toolbar.active_multi = range_tool
 
+    plots = (p, select)
+    script, div = components(plots)
+    cdn_js = CDN.js_files[0]
+    cdn_css = CDN.css_files
 
-    # select = figure(title='Selecionar pico',
-    #                 plot_height=130, plot_width=1200, y_range=p.y_range,
-    #                 x_axis_type="linear", y_axis_type=None,
-    #                 tools="", toolbar_location=None, background_fill_color="#efefef")
-    # range_tool = RangeTool(x_range=p.x_range)
-    # range_tool.overlay.fill_color = "navy"
-    # range_tool.overlay.fill_alpha = 0.2
-    # select.line('Angle', 'Det1Disc1', source=df)
-    # select.ygrid.grid_line_color = None
-    # select.xgrid.grid_line_color = None
-    # select.add_tools(range_tool)
-    # select.toolbar.active_multi = range_tool
-
-    # save(column(p, select))
-
-
-
-
-    # return template.render(script=script, div=div)
-
-    return render_template('page3.html')
-
-# @app.route('/')
-# def index():
-#     plot = uploaded_file()
-#     script, div = components(plot)
-#     return render_template("dif.html", script=script, div=div)
-#
+    return render_template('page2.html',
+                           script=script,
+                           div=div,
+                           cdn_js=cdn_js, cdn_css=cdn_css)
 
 
 if __name__ == "__main__":
