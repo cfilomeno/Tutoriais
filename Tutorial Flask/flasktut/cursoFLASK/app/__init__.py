14cfe821a9676f008c87f6480bcb9271f0f035f7
Cleber Filomeno <cleber.filomeno@gmail.com>
2021-02-03 11:56:52 -0300

    aula 4

--- Tutorial Flask/flasktut/cursoFLASK/app/__init__.py
+++ Tutorial Flask/flasktut/cursoFLASK/app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
-@app.route("/")
-def index():
-    return "Hello world!"
-
-
-if __name__ == "__main__":
-    app.run()
+from app.controllers import default
---
+++ Tutorial Flask/flasktut/cursoFLASK/app/models/tables.py
@@ -0,0 +1,46 @@
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+
+    def __init__(self, username, password, name, email):
+        self.username(username)
+        self.password(password)
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return "<User %r>" % self.username
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    user = db.relationship('User', foreign_keys=user_id)
+
+    def __init__(self, content, user_id):
+        self.content = content
+        self.user_id = user_id
+
+    def __repr__(self):
+        return "<Post %r>" % self.id
+
+
+class Follow(db.Model):
+    __tablename__ = "follow"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    follower_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    user = db.relationship('User', foreign_keys=user_id)
+    follower = db.relationship('User', foreign_keys=follower_id)
