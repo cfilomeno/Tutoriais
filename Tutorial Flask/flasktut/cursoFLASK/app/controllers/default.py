b5b18d656886b3b10379d00b25c17b4c8dbba6e3
Cleber Filomeno <cleber.filomeno@gmail.com>
2021-02-03 12:22:54 -0300

    aula 6 flask

--- Tutorial Flask/flasktut/cursoFLASK/app/controllers/default.py
+++ Tutorial Flask/flasktut/cursoFLASK/app/controllers/default.py
@@ -1,7 +1,28 @@
 from app import app
 
 
+@app.route("/index")
 @app.route("/")
 def index():
     return "Hello world!"
 
+
+# @app.route("/test", defaults={'name': None})
+# @app.route("/test/<name>")
+# def test(name=None):
+#     if name:
+#         return "Olá! %s" % name
+#     else:
+#         return "Olá, usuário!"
+#
+
+
+# @app.route("/test/<int:id>")
+# def test(id):
+#     print(type(id))
+#     return "oi"
+
+
+@app.route("/test/", methods=['GET'])
+def test():
+    return "Oi!"
