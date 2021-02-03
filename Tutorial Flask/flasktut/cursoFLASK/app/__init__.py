7a74cee3ae202a09afcc6a162529fafb82302619
Cleber Filomeno <cleber.filomeno@gmail.com>
2021-02-03 18:25:43 -0300

    aula 9 flask

--- Tutorial Flask/flasktut/cursoFLASK/app/__init__.py
+++ Tutorial Flask/flasktut/cursoFLASK/app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
+
 app = Flask(__name__)
 app.config.from_object('config')
 
--- Tutorial Flask/flasktut/cursoFLASK/config.py
+++ Tutorial Flask/flasktut/cursoFLASK/config.py
@@ -3,3 +3,4 @@ DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
+SECRET_KEY = 'um-nome-bem-seguro'
